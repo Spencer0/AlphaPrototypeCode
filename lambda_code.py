50d31124c27c893c1d9ad41fce9a5f1559c222b8
Spencer0 <38340800+Spencer0@users.noreply.github.com>
2019-12-09 15:47:47 -0800

    Capstone - Alpha Example Code

--- lambda_code.py
+++ lambda_code.py
@@ -12,12 +12,12 @@ def lambda_handler(event, context):
     polite_message = "You have uploaded " + datafile_name + ". It's size is " + str(datafile_size) + ".\nIt has been placed into: " + bucket_name
     print(polite_message)
     #My Discords Webhook
-    #webhook = DiscordWebhook(url='https://discordapp.com/api/webhooks/641531129161318410/zgSY79kg8CVZVUAH8WlaFtg91xWzAO9U_q6rjoaawBZQEIIhCu_BJ62b0jRCY0u5S6bV', 
+    #webhook = DiscordWebhook(url='[Secret Redacted]', 
     #content=polite_message)
     #webhook.execute()
     
     #Sobek Webhook
-    webhook = DiscordWebhook(url='https://discordapp.com/api/webhooks/624644412605071386/vH2qW75Dwxw8eM3Km8nGfj7YdttY6pjmXVfo4oT9L49Lt3y-CfUeQd4GeTCukKUT8Uqe', 
+    webhook = DiscordWebhook(url='[Secret Redacted]', 
     content=polite_message)
     webhook.execute()
 
